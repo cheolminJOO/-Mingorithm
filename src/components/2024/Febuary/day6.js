c791478c79f89be7ebf377d2a1556f0868e21fa9
cheolminJOO <cheolmin.joo@gmail.com>
2024-02-06 23:25:24 +0900

    [add] 240206 algorithm

--- src/components/2024/Febuary/day6.js
+++ src/components/2024/Febuary/day6.js
@@ -1,11 +1,30 @@
-function solution(my_string, overwrite_string, s) {
-  var answer = '';
-  const front = my_string.substring(0, s);
-  const back = my_string.substring(overwrite_string.length + s);
+// function solution(my_string, overwrite_string, s) {
+//   var answer = '';
+//   const front = my_string.substring(0, s);
+//   const back = my_string.substring(overwrite_string.length + s);
 
-  answer = front + overwrite_string + back;
+//   answer = front + overwrite_string + back;
 
-  return answer;
-}
+//   return answer;
+// }
 
-solution('He11oWor1d', 'lloWorl', 2);
+// solution('He11oWor1d', 'lloWorl', 2);
+
+// function solution(str1, str2) {
+//   var answer = '';
+//   const aa = str1.split('');
+//   const bb = str2.split('');
+//   const cc = str1 + str2;
+//   const ee = [];
+
+//   for (let i = 0; i < cc.length; i++) {
+//     ee.push(aa[i]);
+//     ee.push(bb[i]);
+//   }
+
+//   answer = ee.join('');
+
+//   return answer;
+// }
+
+// solution('aaaaa', 'bbbbb');
